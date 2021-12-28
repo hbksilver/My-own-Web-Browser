55002d8c4c894b4853e5e36920f2a5e275dd8211
hbarak <hbarak@HASSANBARAK53A3>
2021-12-28 15:02:47 -0800

    Add project files.

---
+++ Browser/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Browser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string WebPage = textBox1.Text.Trim();
+            webBrowser1.Navigate(WebPage);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+            {
+
+                webBrowser1.GoBack();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+            {
+
+                webBrowser1.GoForward();
+            }
+        }
+     
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+    }
+}
